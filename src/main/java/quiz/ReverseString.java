acaa525e89a26659ff9bd08986c07d34af8c8bbf
Micahel Manzanares <michaelmanzanares1994@gmail.com>
2017-12-20 10:49:07 -0500

    Update ReverseString.java
    fixed tabs and added comments

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,11 +1,11 @@
 package quiz;
 
 public class ReverseString {
-    public String reverse(String input) {
-    	StringBuilder build = new StringBuilder();
-		for (int i = input.length()-1; i >= 0; i--) {
-			build.append(input.charAt(i));
+	public String reverse(String input) {
+		StringBuilder build = new StringBuilder();
+		for (int i = input.length() - 1; i >= 0; i--) {
+			build.append(input.charAt(i));	// build the string in reverse
 		}
-        return build.toString();
-    }
+		return build.toString();
+	}
 }

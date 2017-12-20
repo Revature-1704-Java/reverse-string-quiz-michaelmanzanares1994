77ac8581e6607086dde936f980b43d7b49cf359a
Micahel Manzanares <michaelmanzanares1994@gmail.com>
2017-12-20 10:39:59 -0500

    updated ReverseString

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -2,6 +2,10 @@ package quiz;
 
 public class ReverseString {
     public String reverse(String input) {
-        return new StringBuffer(input).reverse().toString();
+    	String str= "";
+		for (int i = input.length()-1; i >= 0; i--) {
+			str += input.charAt(i);
+		}
+        return new StringBuffer(str).reverse().toString();
     }
 }
